ef954a314405069d1099aa0adc5b17dcc36a7917
surabhisnath <surabhi16271@iiitd.ac.in>
2018-04-11 22:33:23 +0530

    Done!

--- ques1.c
+++ ques1.c
@@ -1,52 +1,67 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <string.h>
+#include <unistd.h>
 
-sem_t mutex, writeblock;
-int data = 0, read_count = 0;
+sem_t mutex[40], writeblock[40];
+sem_t writer_check;
+int data[40];
+//memset(data, 0 , 40*sizeof(data[0]));
+int read_count[40];
+//memset(read_count, 0, 40*sizeof(read_count[0]));
 
 
 void *reader(void *arg)
 {
   int f;
   f = ((int)arg);
-  sem_wait(&mutex);
-  read_count++;
+  int index=(rand())%40;
+  sem_wait(&mutex[index]);
+  read_count[index]++;
   
-  if(read_count==1)
+  if(read_count[index]==1)
   {
-    sem_wait(&writeblock);
+    sem_wait(&writeblock[index]);
   }
 
-  sem_post(&mutex);
+  sem_post(&mutex[index]);
+  
+  printf("Data read by the reader%d is %d at index %d\n",f,data[index],index);
+  
 
-  printf("Data read by the reader%d is %d\n",f,data);
+
+
   sleep(1);
   
-  sem_wait(&mutex);
+  sem_wait(&mutex[index]);
   
-  read_count--;
+  read_count[index]--;
   
-  if(read_count==0)
+  if(read_count[index]==0)
   {
-    sem_post(&writeblock);
+    sem_post(&writeblock[index]);
   }
 
-  sem_post(&mutex);
+  sem_post(&mutex[index]);
 }
 
 
 void *writer(void *arg)
 {
+
+  sem_wait(&writer_check);
   int f;
   f = ((int) arg);
-  sem_wait(&writeblock);
-  data++;
-
-  printf("Data writen by the writer%d is %d\n",f,data);
+  int index=(rand())%40;
+  int num=(rand()+10)%100;
+  sem_wait(&writeblock[index]);
+  data[index]=num;
+  printf("Data writen by the writer%d is %d at index %d\n",f,data[index],index);
   
   sleep(1);
-  sem_post(&writeblock);
+  sem_post(&writeblock[index]);
+  sem_post(&writer_check);
 }
 
 
@@ -54,25 +69,29 @@ void *writer(void *arg)
 int main()
 {
   int i,b; 
-  pthread_t rtid[5],wtid[5];
+  pthread_t rtid[20],wtid[20];
+  memset(data, 0 , 40*sizeof(data[0]));
+  memset(read_count, 0, 40*sizeof(read_count[0]));
+  sem_init(&writer_check, 0, 1);
 
-  sem_init(&mutex,0,1);
-  sem_init(&writeblock,0,1);
+  for(int j=0; j<40; j++)
+  {
 
-  for(i=0;i<5;i++)
+    sem_init(&mutex[j],0,1);
+    sem_init(&writeblock[j],0,1);
+ 
+  }
+
+  for(i=0;i<20;i++)
   {
     pthread_create(&wtid[i],NULL,writer,(void *)i);
     sleep(1);
     pthread_create(&rtid[i],NULL,reader,(void *)i);
-  }
-
-  for(int i=0; i<5; i++)
-  {
-    sleep(2);
+    //sleep(1);
   }
 
 
-  for(i=0;i<5;i++)
+  for(i=0;i<20;i++)
   {
     pthread_join(wtid[i],NULL);
     pthread_join(rtid[i],NULL);
